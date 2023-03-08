36f1af859d9a69b5f711c644fe81b58bdeab74b6
Oleg <shuka.21@mail.ru>
2023-03-08 09:48:55 +0300

    homework

---
+++ new/HW8070323/case1/Program.cs
@@ -0,0 +1,58 @@
+﻿int arrayRows = 3;
+int arrayColumns = 4;
+int arrayMaxValue = 10;
+int arrayMinValue = 1;
+
+Console.WriteLine("Исходный массив:");
+int[,] array = ArrayGenerator(arrayRows, arrayColumns, arrayMaxValue, arrayMinValue);
+
+for (int currentStringIndex = 0; currentStringIndex < arrayRows; currentStringIndex++)
+{
+    SortStrOfArray(array, currentStringIndex);
+}
+
+Console.WriteLine("\nРезультат: ");
+for (int i = 0; i < arrayRows; i++)
+{
+
+    for (int j = 0; j < arrayColumns; j++)
+    {
+        Console.Write(array[i, j] + "\t|");
+    }
+    Console.WriteLine("<-- row " + (i + 1));
+}
+
+
+void SortStrOfArray(int[,] array, int currentStringIndex)
+{
+    int tempMemory;
+    for (int i = 0; i < array.GetLength(1); i++)
+    {
+        for (int j = array.GetLength(1) - 1; j > i; j--)
+        {
+            if (array[currentStringIndex, j] < array[currentStringIndex, j - 1])
+            {
+                tempMemory = array[currentStringIndex, j];
+                array[currentStringIndex, j] = array[currentStringIndex, j - 1];
+                array[currentStringIndex, j - 1] = tempMemory;
+            }
+        }
+    }
+}
+
+
+
+int[,] ArrayGenerator(int lenghtRows, int lenghtColum, int maxValue, int minValue) // рандом генератор
+{
+    int[,] array = new int[lenghtRows, lenghtColum];
+    for (int i = 0; i < lenghtRows; i++)
+    {
+        for (int j = 0; j < lenghtColum; j++)
+        {
+            array[i, j] = new Random().Next(minValue, maxValue);
+            Console.Write(array[i, j] + "\t|");
+        }
+        Console.WriteLine("<-- row " + (i + 1));
+    }
+    return array;
+}
---
+++ new/HW8070323/case3/Program.cs
@@ -0,0 +1,38 @@
+﻿int arrayRows = 4;
+int arrayColumns = 4;
+int arrayMaxValue = 10;
+int arrayMinValue = 1;
+
+Console.WriteLine("Исходный массив :");
+int[,] matrixA = ArrayGenerator(arrayRows, arrayColumns, arrayMaxValue, arrayMinValue);
+
+Console.WriteLine("\nИсходный массив B:");
+int[,] matrixB = ArrayGenerator(arrayRows, arrayColumns, arrayMaxValue, arrayMinValue);
+
+int[,] resultArray = new int[arrayRows, arrayColumns];
+
+Console.WriteLine("\nРезультат:");
+for (int i = 0; i < arrayRows; i++)
+{
+    for (int j = 0; j < arrayColumns; j++)
+    {
+        resultArray[i, j] = matrixA[i, j] * matrixB[i, j];
+        Console.Write(resultArray[i, j] + "\t|");
+    }
+    Console.WriteLine("<-- row " + (i + 1));
+}
+
+int[,] ArrayGenerator(int lenghtRows, int lenghtColum, int maxValue, int minValue) // рандом генератор
+{
+    int[,] array = new int[lenghtRows, lenghtColum];
+    for (int i = 0; i < lenghtRows; i++)
+    {
+        for (int j = 0; j < lenghtColum; j++)
+        {
+            array[i, j] = new Random().Next(minValue, maxValue);
+            Console.Write(array[i, j] + "\t|");
+        }
+        Console.WriteLine("<-- row " + (i + 1));
+    }
+    return array;
+}
