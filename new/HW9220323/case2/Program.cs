a2b5b8668229e9f93f142ebad400ff360cfa279c
Oleg <shuka.21@mail.ru>
2023-03-22 11:56:20 +0300

    homework

---
+++ new/HW9220323/case2/Program.cs
@@ -0,0 +1,14 @@
+﻿int m, n;
+Console.Write("Введите начальное значение M: ");
+m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите конечное значение N: ");
+n = Convert.ToInt32(Console.ReadLine());
+
+int SumRange(int start, int end)
+{
+    int sum = 0;
+    if (start < end) sum = start + end + SumRange(start + 1, end - 1);
+    else if (start == end) sum = end;
+    return sum;
+}
+Console.WriteLine(SumRange(m, n));
---
+++ new/HW9220323/case3/Program.cs
@@ -0,0 +1,15 @@
+﻿int m, n;
+Console.Write("Введите начальное значение M: ");
+m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите конечное значение N: ");
+n = Convert.ToInt32(Console.ReadLine());
+
+int Akkerman(int m, int n)
+{
+    //Console.WriteLine("M = " + m + "; N = " + n);
+    if (m > 0 && n == 0) return Akkerman(m - 1, 1);
+    if (m > 0 && n > 0) return Akkerman(m - 1, Akkerman(m, n - 1));
+    return n + 1;
+}
+
+Console.WriteLine(Akkerman(m, n));
